570bc5b0ebd7ea9652018b83906f77f5f37bdab8
kariuki26578-pixel <kariuki.26578@students.kyu.ac.ke>
2025-11-07 12:17:08 +0300

    week 1

---
+++ semester_units.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Units Offered This Semester:\n");
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Architecture\n");
+    printf("4. Operating Systems\n");
+    printf("5. Database Management Systems\n");
+
+    return 0;
+
+}
